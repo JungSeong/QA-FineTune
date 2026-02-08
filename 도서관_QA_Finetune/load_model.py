53a4c1af16791c59658f5b97c011d4504357e5e1
JungSeong <jungseonglian@sju.ac.kr>
2026-02-09 00:20:48 +0900

    260208 - 다중 요청 vLLM 추론 성공

--- 도서관_QA_Finetune/load_model.py
+++ 도서관_QA_Finetune/load_model.py
@@ -1,10 +1,13 @@
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+
 from huggingface_hub import snapshot_download
-save_path = "/home/vsc/LLM/model/Exaone-3.5-32B-Instruct"
+
+save_path = "/home/vsc/LLM/model/Exaone-3.5-7.8B-Instruct-AWQ"
 
 snapshot_download(
-    repo_id="LGAI-EXAONE/EXAONE-3.5-32B-Instruct",
+    repo_id="LGAI-EXAONE/EXAONE-3.5-7.8B-Instruct-AWQ",
     local_dir=save_path,
     local_dir_use_symlinks=False,  # 실제 파일을 해당 경로에 직접 복사/다운로드
-    resume_download=True, # 연결 중단 지점부터 다시 다운로드
     max_workers=8 # 병렬 다운로드 속도 향상
 )
